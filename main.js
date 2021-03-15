a6234e5ebf18484025798a2049422c93b8f45977
Victor Okonkwo <emmyvic98@gmail.com>
2021-03-15 22:03:31 +0100

    L4-challenge-complete

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            onSale: false,
+            onHold: false
         }
     }
 })
